adcda046f026ff417a128ad771301aef048ec03a
Siyan Guo <siyang@Hey-There.local>
2021-08-22 21:37:00 -0400

    handle request for a specific animal

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ function filterByQuery(query, animalsArray) {
             personalityTraitsArray = query.personalityTraits;
         }
     }
-      // Loop through each trait in the personalityTraits array:
-      // Check the trait against each animal in the filteredResults array.
-      // Remember, it is initially a copy of the animalsArray, but here we're updating it for each trait in the .forEach() loop.
-      // For each trait being targeted by the filter, the filteredResults array will then contain only the entries that contain the trait,
-      // so at the end we'll have an array of animals that have every one of the traits when the .forEach() loop is finished.
+    // Loop through each trait in the personalityTraits array:
+    // Check the trait against each animal in the filteredResults array.
+    // Remember, it is initially a copy of the animalsArray, but here we're updating it for each trait in the .forEach() loop.
+    // For each trait being targeted by the filter, the filteredResults array will then contain only the entries that contain the trait,
+    // so at the end we'll have an array of animals that have every one of the traits when the .forEach() loop is finished.
     personalityTraitsArray.forEach(trait => {
         filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1);
     });
@@ -37,6 +37,15 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
+// function findById(name, animalsArray) {
+//     const result = animalsArray.filter(animal => animal.name === name)[0];
+//     return result;
+// };
 
 app.get('/api/animals', (req, res) => {
     // res.send('Hello!')
@@ -47,6 +56,25 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    console.log(req.params);
+    const result = findById(req.params.id, animals);
+    if (result){
+        res.json(result);
+    } else {
+        res.sendStatus(404)
+    }
+});
+
+// app.get('/api/animals/:name', (req, res) => {
+//     const result = findById(req.params.name, animals);
+//     if (result){
+//         res.json(result);
+//     } else {
+//         res.sendStatus(404)
+//     }
+// });
 
 app.listen(PORT, () => {
     console.log(`App server now on ${PORT}!`);
