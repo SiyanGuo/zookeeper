af7956839cd27df1702efc899f0fa183b6d0f387
Siyan Guo <siyang@Hey-There.local>
2021-08-24 20:56:41 -0400

    create data using POST route

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
 const { animals } = require('./data/animals');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let filteredResults = animalsArray;
@@ -42,11 +50,54 @@ function findById(id, animalsArray) {
     return result;
 };
 
-// function findById(name, animalsArray) {
-//     const result = animalsArray.filter(animal => animal.name === name)[0];
-//     return result;
-// };
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    const animal = body;
+    animalsArray.push(animal);
 
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals:animalsArray}, null, 2)
+    );
+
+    return animal;
+}
+
+
+function validateAnimal(animal){
+    if(!animal.name || typeof animal.name !=='string'){
+        return false;
+    }
+    if(!animal.species || typeof animal.species !== 'string'){
+        return false;
+    }
+    if(!animal.diet || typeof animal.diet !=='string'){
+        return false;
+    }
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+    return true;
+}
+
+
+//create data using POST route
+app.post('/api/animals', (req, res) => {
+    //req.body is where our incoming content will be
+    //set id based on what the next index of the array will be 
+    req.body.id = animals.length.toString();
+
+    //if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)){
+        res.status(400).send('The animal is not properly formatted.')
+    } else {
+        //add animal to JSON file 
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
+
+//handle requests for animals
 app.get('/api/animals', (req, res) => {
     // res.send('Hello!')
     let results = animals;
@@ -57,24 +108,17 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+//handle requests for a specific animal
 app.get('/api/animals/:id', (req, res) => {
     console.log(req.params);
     const result = findById(req.params.id, animals);
-    if (result){
+    if (result) {
         res.json(result);
     } else {
         res.sendStatus(404)
     }
 });
 
-// app.get('/api/animals/:name', (req, res) => {
-//     const result = findById(req.params.name, animals);
-//     if (result){
-//         res.json(result);
-//     } else {
-//         res.sendStatus(404)
-//     }
-// });
 
 app.listen(PORT, () => {
     console.log(`App server now on ${PORT}!`);
